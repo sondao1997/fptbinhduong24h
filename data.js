77c1ddb2739ca55db68cb650449d3923cd9544b0
Timap <32201684+sondao1997@users.noreply.github.com>
2021-08-04 18:48:01 +0700

    add new price list - index

---
+++ data.js
@@ -0,0 +1,55 @@
+export const dataListPrice = {
+  //Thủ dầu, dĩ an, thuận an
+     thuDau_diAn_thuanAn: [
+       {
+         internet: {
+           super30: 220000,
+           super80: 240000,
+           super100: 260000,
+           super150: 350000,
+         },
+       },
+       {
+         comboInternet: {
+           super30: 220000,
+           super80: 240000,
+           super100: 260000,
+           super150: 350000,
+         },
+       },
+       {
+         comboInternet: {
+           super30: 220000,
+           super80: 240000,
+           super100: 260000,
+           super150: 350000,
+         },
+       },
+     ],
+     benCat: [
+       {
+         internet: {
+           super30: 220000,
+           super80: 240000,
+           super100: 260000,
+           super150: 350000,
+         },
+       },
+       {
+         comboInternet: {
+           super30: 220000,
+           super80: 240000,
+           super100: 260000,
+           super150: 350000,
+         },
+       },
+       {
+         comboInternet: {
+           super30: 220000,
+           super80: 240000,
+           super100: 260000,
+           super150: 350000,
+         },
+       },
+     ],
+};
---
+++ index.js
@@ -0,0 +1,63 @@
+ 
+
+ const dataListPrice = {
+     internet: [
+         {
+             goicuoc:'Super-30',
+             bangthong:'30 Mbps',
+             cuocthang:'190.000'
+         },
+         {
+             goicuoc:'Super-30',
+             bangthong:'30 Mbps',
+             cuocthang:'190.000'
+         },
+         {
+             goicuoc:'Super-30',
+             bangthong:'30 Mbps',
+             cuocthang:'190.000'
+         },
+         {
+             goicuoc:'Super-30',
+             bangthong:'30 Mbps',
+             cuocthang:'190.000'
+         }
+     ],
+     combo:[
+        {
+            goicuoc:'Super-45', 
+            cuocthang:'240.000'
+        },
+        {
+            goicuoc:'Super-30', 
+            cuocthang:'260.000'
+        },
+        {
+            goicuoc:'Super-30', 
+            cuocthang:'350.000'
+        }
+     ],
+     internetDN:[
+        {
+            goicuoc:'Super-150',
+            bangthong:'150 Mbps',
+            cuocthang:'190.000'
+        },
+        {
+            goicuoc:'Super-200',
+            bangthong:'200 Mbps',
+            cuocthang:'190.000'
+        },
+        {
+            goicuoc:'Super-250',
+            bangthong:'250 Mbps',
+            cuocthang:'190.000'
+        },
+        {
+            goicuoc:'Super-300',
+            bangthong:'300 Mbps',
+            cuocthang:'190.000'
+        }
+     ]
+ }
+ 
